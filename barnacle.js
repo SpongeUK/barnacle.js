a3f1081218de3e6aa3dc72ab1343f5a35fa8b7a6
Chris Jones <chris.jones@spongeuk.com>
2012-12-06 10:36:42 +0000

    console debugging

--- barnacle.js
+++ barnacle.js
@@ -44,7 +44,7 @@
   };
 
   scorm.setup = function (opts){
-    options.debug = !console ? false : opts.debug;
+    options.debug = !window.console ? false : opts.debug;
     options.onCommit = opts.onCommit;
     options.hasBeenSetup = true;
   };
